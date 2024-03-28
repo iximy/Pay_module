a1151afa9bb59b18a8d77ce979e78c52c3832bdc
iximy <prcr98@gmail.com>
2024-03-28 15:53:11 +0300

    ver 117

--- success.php
+++ success.php
@@ -1,19 +1,15 @@
 <?php 
-//ver 1.16v
+//ver 1.17e
 include 'config.php';
 
 $stmtto = $connpdo->prepare("SELECT * FROM orders WHERE `user` = ? ORDER BY idorder DESC LIMIT 1");
-
 $roworder = $stmtto->fetch(PDO::FETCH_LAZY);
-
 $order_id = $roworder["date"];
- 
  
  
  
 $ch = curl_init('https://api.yookassa.ru/v3/payments/' . $order_id);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_USERPWD, 'id:key');
 curl_setopt($ch, CURLOPT_HEADER, false);
